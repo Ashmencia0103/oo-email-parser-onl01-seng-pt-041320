78da4e397d397472139143f11c64370116a280af
Ashlen Mencia <ashmencia0103@gmail.com>
2020-05-12 21:35:06 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,11 +9,11 @@ class EmailAddressParser
   def initialize(emails)
     @emails = emails
   end
-
-  def parse
-    puts splitEmail = emails.split.collect {|address| address.split(',')}
-    returnArr = splitEmail.flatten.uniq
-    returnArr
-  end
+  #
+  # def parse
+  #   puts splitEmail = emails.split.collect {|address| address.split(',')}
+  #   returnArr = splitEmail.flatten.uniq
+  #   returnArr
+  # end
 
 end
